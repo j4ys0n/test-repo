a818f343650b6720dcc955122d577ac7735abee0
Jayson <j4ys0n@gmail.com>
2018-03-08 09:50:53 -0700

    accept any path

--- node/src/server.ts
+++ node/src/server.ts
@@ -52,7 +52,7 @@ if (process.env.NODE_ENV === 'production') {
 } else {
 	console.log(path.join(__dirname, '..', 'public', 'index.html'));
 	app.use('/', express.static(path.join(__dirname, '..', 'public')));
-	app.get('/', (req, res) => res.sendFile(path.join(__dirname, '..', 'public', 'index.html')));
+	app.get('*', (req, res) => res.sendFile(path.join(__dirname, '..', 'public', 'index.html')));
 	// app.get('/:url', (req, res) => (res.redirect('http://localhost:3001/' + req.params.url)));
 }
 
